871e8e8d0238f732f366ae16cb4f644046cecf46
JhonRoman0 <jhonromanquito@gmail.com>
2025-07-21 13:25:59 -0500

    Update CategoriaController.java

--- src/main/java/com/example/DyD_Natures/Controller/CategoriaController.java
+++ src/main/java/com/example/DyD_Natures/Controller/CategoriaController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import com.itextpdf.text.*; // Importar iText
+import com.itextpdf.text.*; 
 import com.itextpdf.text.pdf.PdfPCell; // Importar iText
 import com.itextpdf.text.pdf.PdfPTable; // Importar iText
 import com.itextpdf.text.pdf.PdfWriter; // Importar iText
